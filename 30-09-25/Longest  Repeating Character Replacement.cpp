4e902303a25aa4f5dc3a99a9def224acf955f231
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-10-01 10:18:21 +0530

    Create Longest  Repeating Character Replacement.cpp

---
+++ 30-09-25/Longest  Repeating Character Replacement.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> count(26, 0);
+        int l= 0, maxl= 0, maxc = 0;
+
+        for (int r= 0; r < s.size(); r++) {
+            char ch = s[r];
+            count[ch - 'A']++;
+            maxcount = max(maxc, count[ch - 'A']);
+
+            while (r - l + 1 - maxc > k) {
+                count[s[l] - 'A']--;
+                l++;
+            }
+
+            maxlen = max(maxl, r - l+ 1);
+        }
+
+        return maxl;
+    }
+};
