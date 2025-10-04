c7f72b59246361d3873fa973664732054e24316d
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-10-04 23:43:33 +0530

    Create Maximum Subarray.java

---
+++ 04-10-25/Maximum Subarray.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxsum = Integer.MIN_VALUE;
+
+        for (int num : nums) {
+            sum += num;
+            maxsum = Math.max(maxsum, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxsum;
+    }
+}
