9ece9eca3aaac60015136f965af92359db59339f
VanshikaVarshney03 <vanshikavarshney0311@gmail.com>
2025-10-26 15:18:11 +0530

    Create Maximal Recetangle.java

---
+++ 21-10-25/Maximal Recetangle.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+           if (matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return 0;
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int[] r = new int[m]; 
+
+            for (int j = i; j < n; j++) {
+
+                
+                for (int k = 0; k < m; k++) {
+                    r[k] += matrix[j][k] - '0';
+                }
+
+                int h = j - i + 1; 
+                int sum = 0;
+
+                for (int k = 0; k < m; k++) {
+                    if (r[k] == h) {
+                        sum += h;
+                        ans = Math.max(ans, sum);
+                    } else {
+                        sum = 0;
+                    }
+                }
+            }
+        }
+
+        return ans;
+    }
+}
